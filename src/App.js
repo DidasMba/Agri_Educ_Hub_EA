64c9b948a68f9a621f1a5e784fc68a4076321588
DidasMba <daniel.mbarushi@gmail.com>
2023-12-25 15:05:37 +0300

    editing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Router } from 'react-router-dom';
+//import logo from './logo.svg';
+//import './App.css';
+//import { Route, Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
+// Define the main component of the application named 'App'
 function App() {
   return (
+    // use the 'Router' component from 'react-router-dom' to enable routing
     <Router>
-      <switch>
+      {/* Use the 'Switch' component to render only the first matching route */}
+    
+        {/* Define a route for the home page, exact ensures an exact match */}
+        <Route path="/" element={<Home />} />
         <Route/>
         <Route/>
-        <Route/>
-      </switch>
+  
     </Router>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,12 @@ import React from "react";
 function Home(){
     return(
         <div>
-            <h1>Home</h1>
-            {/* Home page content */}
-        </div>
-    )
+        <h1>Welcome to AgriLearn Africa!</h1>
+        <p>Empowering farmers through education and knowledge sharing.</p>
+        {/* Additional content or features */}
+      </div>
+  
+    );
 }
+
+export default Home;
