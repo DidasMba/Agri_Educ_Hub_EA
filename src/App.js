71f43c31bd9d0015ccd89651b798bca0952d4adb
DidasMba <daniel.mbarushi@gmail.com>
2023-12-25 10:11:31 +0300

    first edit App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <switch>
+        <Route/>
+        <Route/>
+        <Route/>
+      </switch>
+    </Router>
+  );
+}
+
+export default App;
